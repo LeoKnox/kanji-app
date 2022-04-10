c14457d7ebac912431af40aa695fc403e095ba7b
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-10 15:18:54 -0700

    Add files via upload

---
+++ src/withKanji.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from 'react';
+
+export const withUser = (Component, userId) => {
+    return props => {
+        const [kanji, setKanji] = useState(null);
+
+        useEffect(() => {
+            (async () => {
+                //const respoonse = await axios.get(`/users/${userId}`);
+                const response = [
+                    {idkanji_dict:1, kanji:"一", strokes:1, meaning:"one",prounciation:"ichi, itsu",reading:"hito-tsu",grade:1}
+                ];
+                setKanji(response.data);
+        })();
+    });
+
+    return <Component {...props} kanji={kanji} />
+    }
+}
