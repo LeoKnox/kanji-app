f5936ddba4f037d65ba40f3460371bd0669268bd
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-11 17:04:04 -0700

    Add files via upload

--- src/data.js
+++ src/data.js
@@ -1,3 +1,10 @@
 const data = [
-    {idkanji_dict:1, kanji:"一", strokes:1, meaning:"one",prounciation:"ichi, itsu",reading:"hito-tsu",grade:1}
+    {idkanji_dict:1, kanji:"一", strokes:1, meaning:"one",prounciation:"ichi, itsu",reading:"hito-tsu",grade:1},
+    {idkanji_dict:2, kanji:"二", strokes:2, meaning:"two",prounciation:"ichi, itsu",reading:"hito-tsu",grade:1},
+    {idkanji_dict:3, kanji:"三", strokes:3, meaning:"three",prounciation:"ichi, itsu",reading:"hito-tsu",grade:1},
+    {idkanji_dict:4, kanji:"四", strokes:5, meaning:"four",prounciation:"ichi, itsu",reading:"hito-tsu",grade:1},
+    {idkanji_dict:5, kanji:"五", strokes:4, meaning:"five",prounciation:"ichi, itsu",reading:"hito-tsu",grade:1},
+    {idkanji_dict:6, kanji:"五", strokes:4, meaning:"six",prounciation:"ichi, itsu",reading:"hito-tsu",grade:1},
+    {idkanji_dict:7, kanji:"七", strokes:2, meaning:"seven",prounciation:"ichi, itsu",reading:"hito-tsu",grade:1},
+    {idkanji_dict:8, kanji:"八", strokes:2, meaning:"eight",prounciation:"ichi, itsu",reading:"hito-tsu",grade:1},
 ];
