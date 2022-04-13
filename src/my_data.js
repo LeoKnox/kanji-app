77761d202941a5436ccecba661708654ca56e2b2
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-13 11:56:38 -0700

    Add files via upload

--- src/my_data.js
+++ src/my_data.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import data from './data.json';
 
 data.sort(() => Math.random() - 0.5)
-const datatwo = data.slice(0,3);
+const datatwo = data.slice(0,6);
 
 function Single() {
     return (
         datatwo.map((d, i) =>
-            <p key={i}> {d.kanji} : {d.meaning}</p>
+            <p key={i} className="kanjiCube">{d.kanji}</p>
         )
     )
 }
