0af40e181cc52cb7361a2862793cbd30d240cf9b
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-13 11:01:17 -0700

    Add files via upload

--- src/my_data.js
+++ src/my_data.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import data from './data.json';
 
-class Single extends Component {
-    render() {
-        return (
-            this.data = data.map((d) =>
-                <p>{d.kanji} : {d.meaning}</p>
-            )
+const datatwo = [{"idkanji_dict":1, "kanji":"一", "strokes":1, "meaning":"one","prounciation":"ichi, itsu","reading":"hito-tsu","grade":1},
+{"idkanji_dict":2, "kanji":"二", "strokes":2, "meaning":"two","prounciation":"ni, ji","reading":"futa-tsu","grade":1}]
+
+function Single() {
+    return (
+        datatwo.map((d, i) =>
+            <p key={i}> {d.kanji} : {d.meaning}</p>
         )
-    }
+    )
 }
 
 export default Single;
