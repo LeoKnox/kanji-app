cd7d32c6a55d1b9c94b32769a2970ab713117052
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-13 11:50:57 -0700

    Add files via upload

--- src/my_data.js
+++ src/my_data.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import data from './data.json';
 
-const datatwo = [{"idkanji_dict":1, "kanji":"一", "strokes":1, "meaning":"one","prounciation":"ichi, itsu","reading":"hito-tsu","grade":1},
-{"idkanji_dict":2, "kanji":"二", "strokes":2, "meaning":"two","prounciation":"ni, ji","reading":"futa-tsu","grade":1}]
+data.sort(() => Math.random() - 0.5)
+const datatwo = data.slice(0,3);
 
 function Single() {
     return (
