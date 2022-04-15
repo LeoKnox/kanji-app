1b28b1f41265833d7154f7033ecf91bc4107ee67
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-15 09:40:02 -0700

    Add files via upload

--- src/my_data2.js
+++ src/my_data2.js
@@ -4,11 +4,12 @@ import data from './data.json';
 function kanjiMap() {
     data.sort(() => Math.random() - 0.5)
     const datatwo = data.slice(0,6);
+    console.log("red");
 
     return (
-        <div>
-            {datatwo.map((k) => (
-                <p>{k.kanji}</p>
+        <div className="kanjiWrapper">
+            {datatwo.map((k, i) => (
+                <p key={i} onClick={kanjiMap} className="kanjiCube">{k.kanji}</p>
             ))}
         </div>
     )
