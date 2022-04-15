162a79f142479a84c0bce8fb56d17b64ac499601
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-15 11:16:13 -0700

    Add files via upload

--- src/my_data2.js
+++ src/my_data2.js
@@ -1,22 +1,24 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEfect } from 'react';
 import data from './data.json';
 
 function kanjiTest(myData) {
-    myData.sort(() => Math.random() - 0.5)
+    setKanji(myData.sort(() => Math.random() - 0.5));
     console.log(myData);
     return(data.slice(0,6));
 }
 
-function kanjiMap() {
-    const datatwo = kanjiTest(data);
+function KanjiMap() {
+    const [kanji, setKanji] = useState(kanjiTest(data));
+
+    //const datatwo = kanjiTest(data);
 
     return (
         <div className="kanjiWrapper">
-            {datatwo.map((k, i) => (
-                <p key={i} onClick={kanjiMap} className="kanjiCube">{k.kanji}</p>
+            {kanji.map((k, i) => (
+                <p key={i} onClick={setKanji(data)} className="kanjiCube">{k.kanji}</p>
             ))}
         </div>
     )
 }
 
-export default kanjiMap;
+export default KanjiMap;
