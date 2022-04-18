eb1d6f7a0286d221da903d34e926de497b418186
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-17 19:22:17 -0700

    Add files via upload

--- src/my_data3.js
+++ src/my_data3.js
@@ -5,13 +5,15 @@ function KanjiMap() {
     const [kanji, setKanji] = useState(data);
 
     useEffect(() => {
-        setKanji(data.sort(() => Math.random() - 0.5).slice(0,6))
+        return () => {
+            setKanji(data.sort(() => Math.random() - 0.5).slice(0,6))
+        }
     }, [])
 
     return (
         <div className="kanjiWrapper">
             {kanji.map((k, i) => (
-                <p key={i} onClick={setKanji(data)} className="kanjiCube">{k.kanji}</p>
+                <p key={i} onClick={useEffect} className="kanjiCube">{k.kanji}</p>
             ))}
         </div>
     )
