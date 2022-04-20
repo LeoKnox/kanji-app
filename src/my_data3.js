adfdef014da044897a8770d6bea8039e1e76848d
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-19 20:49:10 -0700

    Add files via upload

--- src/my_data3.js
+++ src/my_data3.js
@@ -17,19 +17,14 @@ function KanjiMap() {
         return (kanji[i].meaning);
     }
     
-    function newKanji() {
-        //console.log(i)
-        console.log(kanji);
-        setKanji(data.sort(() => Math.random() - 0.5).slice(0,6));
-        //setAns(kanji[i]);
-        console.log("ddd"+makeAns());
-        //setAns(kanji[Math.floor(Math.random()*kanji.length)]);
-        //console.log("blue");
-    }
-    
     const changeKanji = (x) => {
-        newKanji();
-        console.log(kanji);
+        let newData = data.sort(() => Math.random() - 0.5).slice(0,6);
+        setAns(newData)
+        setKanji(newData);
+        //setAns(kanji[i]);
+        console.log(newData);
+        setAns(newData[Math.floor(Math.random()*kanji.length)]);
+        //console.log("blue");
     }
 
             //<h3 className="answer">Meaning: {ans.meaning}</h3>  from return
@@ -38,7 +33,7 @@ function KanjiMap() {
             {kanji.map((k, i) => (
                 <p key={i} onClick={() => changeKanji(k.meaning)} className="kanjiCube">{k.kanji}</p>
             ))}
-            <KanjiMap2 meaning="one" />
+            <KanjiMap2 meaning={ans.meaning} />
         </div>
     )
 }
