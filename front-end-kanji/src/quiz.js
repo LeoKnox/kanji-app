62f9322097ae6205740b80741fa4b25b8bd27681
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-22 11:59:15 -0700

    Add files via upload

--- front-end-kanji/src/quiz.js
+++ front-end-kanji/src/quiz.js
@@ -1,16 +1,46 @@
 import React, { Component, useState, useEffect } from 'react';
 import axios from 'axios';
 
-const baseURL = "http://127.0.0.1:8000/";
-
 function KanjiMap() {
+    const [kanji, setKanji] = useState();
+    const [ans, setAns] = useState();
+
     axios.get("http://127.0.0.1:8000/")
         .then((response) => {
-            console.log(response)
+            setKanji(response);
         })
         .catch(function (error) {
             console.log(error);
         })
+
+    useEffect(() => {
+        return () => {
+            let data = kanji;
+            let i = data.sort(() => Math.random() - 0.5).slice(0,6);
+            setKanji(i);
+            setAns(i[Math.floor(Math.random()*kanji.length)]);
+        }
+    }, [])
+
+    const changeKanji = (x) => {
+        if (x === ans.kanji) {
+            let newData = kanji.sort(() => Math.random() - 0.5).slice(0,6);
+            setAns(newData)
+            setKanji(newData);
+            setAns(newData[Math.floor(Math.random()*kanji.length)]);
+        }
+    }
+
+    return (
+        <>
+        <h3 className="answer">Select Kanji for {ans.meaning}</h3>
+        <div className="kanjiWrapper">
+            {kanji.map((k, i) => (
+                <p key={i} onClick={() => changeKanji(k.kanji)} className="kanjiCube">{k.kanji}</p>
+            ))}
+        </div>
+        </>
+    )
 }
 
 export default KanjiMap;
