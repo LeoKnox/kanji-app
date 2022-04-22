cc76266ce3c9e48ec210567d326eff057cc20202
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-21 21:47:58 -0700

    Add files via upload

--- kanji_project/kanji_quiz/views.py
+++ kanji_project/kanji_quiz/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import KanjiListSerializer
+from .serializers import KanjiListSerializer, KanjiGradeSerializer
 from .models import Kanji
 
 class KanjiListAPIView(generics.ListAPIView):
+    print(Kanji.objects.values('grade').distinct())
     queryset = Kanji.objects.all()
     serializer_class = KanjiListSerializer
