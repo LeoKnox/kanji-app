fbe6c08d3fb3e681aa0e9e0043a9fde26f5c454d
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-24 17:05:13 -0700

    Add files via upload

--- kanji_project/kanji_quiz/quiz.js
+++ kanji_project/kanji_quiz/quiz.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 
 const KanjiMap = () => {
     const [kanji, setKanji] = useState([]);
+    const [quiz, setQuiz] = useState([]);
     const [loading, setLoading] = useState(false);
     
     useEffect(() => {
         axios.get('http://127.0.0.1:8000/') 
             .then(function (response) {
-                const selectKanji = response.data.sort(() => Math.random() - 0.5).slice(0,6);
+                const selectKanji = response.data;
                 setKanji(selectKanji);
             })
             .catch(function (error) {
@@ -17,13 +18,13 @@ const KanjiMap = () => {
     }, []);
 
     function changeKanji() {
-        console.log('red');
+        console.log(" ");
     }
 
     return (
         <>
         <div className="kanjiWrapper">
-            {kanji.map((k, i) => (
+            {kanji.sort(() => Math.random() - 0.5).slice(0,6).map((k, i) => (
                 <p key={i} onClick={() => changeKanji(k.kanji)} className="kanjiCube">{k.kanji}</p>
             ))}
         </div>
