df78cccab47b90e2d9bfdd035e0e4b3cbaf2fe4f
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-25 11:39:27 -0700

    Add files via upload

--- kanji_project/kanji_quiz/randomKanji.js
+++ kanji_project/kanji_quiz/randomKanji.js
@@ -1,12 +1,14 @@
 import AnswerKanji from './answerKanji.js';
+import { useState, useEffect } from 'react';
 
 const RandomKanji = (kanji) => {
     const displayKanji = kanji.kanji.sort(() => Math.random() - 0.5).slice(0,6);
-    console.log(displayKanji);
+    const [ans, setAns] = useState(displayKanji[Math.floor(Math.random()*displayKanji.length)].kanji);
+    console.log(ans);
 
     return (
         <>
-        <AnswerKanji />
+        <h1>Answer: {ans}</h1>
         {displayKanji.map((k, i) => (
             <p key={i} onClick="" className="kanjiCube">{k.kanji}</p>
         ))}
