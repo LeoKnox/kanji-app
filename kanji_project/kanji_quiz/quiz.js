4c19882ced355dd2a72f81b1a10b68a61ce6efe3
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-26 16:52:49 -0700

    Add files via upload

--- kanji_project/kanji_quiz/quiz.js
+++ kanji_project/kanji_quiz/quiz.js
@@ -3,35 +3,35 @@ import axios from 'axios';
 import RandomKanji from './randomKanji.js';
 
 const KanjiMap = () => {
-    const [kanji, setKanji] = useState([]);
-    let quiz = [];
-    let ans = "";
-    //let allKanji = []; // had to use because kanji use caused undefined error
-    
-    useEffect(() => {
-        axios.get('http://127.0.0.1:8000/') 
-            .then(function (response) {
-                //kanji = response.data;
-                setKanji(response.data);
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-            .then(function check() {
-                quiz = kanji.sort(() => Math.random() - 0.5).slice(0,6);
-                console.log(quiz);
-                ans = quiz[Math.floor(Math.random()*quiz.length)].kanji;
-                console.log(ans);
-                //setQuiz(quizList);
-                //setAns(quizList[Math.floor(Math.random()*quizList.length)].kanji);
-            })
-    }, []);
+    //const [kanji, setKanji] = useState([]);
+    const [quiz, setQuiz] = useState([]);
+    const [ans, setAns] = useState('');
+    let allKanji = undefined
+        useEffect(() => {
+            axios.get('http://127.0.0.1:8000/') 
+                .then(function (response) {
+                    allKanji = response.data;
+                    //setKanji(response.data);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .then(function check() {
+                    const quizList = allKanji.sort(() => Math.random() - 0.5).slice(0,6);
+                    setQuiz(quizList);
+                    setAns(quizList[Math.floor(Math.random()*quizList.length)].kanji);
+                    return allKanji;
+                })
+
+        }, []);
 
     function check(answer) {
-        if (answer === answer) {
-            quiz = kanji.sort(() => Math.random() - 0.5).slice(0,6);
-            ans = quiz[Math.floor(Math.random()*quiz.length)].kanji;
-            //setQuiz(newQuiz);
+        if (answer === ans) {
+            console.log(allKanji);
+            const newQuiz = allKanji.sort(() => Math.random() - 0.5).slice(0,6);
+            //console.log(newQuiz);
+            setAns(answer);
+            setQuiz(newQuiz);
             //setAns(newQuiz[Math.floor(Math.random()*newQuiz.length)].kanji);
             console.log("Right!");
         } else {
@@ -41,7 +41,7 @@ const KanjiMap = () => {
 
     return (
         <>
-        <h1>Answer: {}</h1>
+        <h1>Answer: {ans}</h1>
         <div className="kanjiWrapper">
             {quiz.map((k, i) => (
                 <p key={i} onClick={() => check(k.kanji)} className="kanjiCube">{k.kanji}</p>
