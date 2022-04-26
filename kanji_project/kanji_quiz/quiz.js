21afa81ed3d8cf267c82f5055063db61ad4d69a5
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-26 11:29:12 -0700

    Add files via upload

--- kanji_project/kanji_quiz/quiz.js
+++ kanji_project/kanji_quiz/quiz.js
@@ -3,21 +3,22 @@ import axios from 'axios';
 import RandomKanji from './randomKanji.js';
 
 const KanjiMap = () => {
-    const [kanji, setKanji] = useState([]);
+    //const [kanji, setKanji] = useState([]);
     const [quiz, setQuiz] = useState([]);
     const [ans, setAns] = useState('');
+    let allKanji = []; // had to use because kanji use caused undefined error
     
     useEffect(() => {
         axios.get('http://127.0.0.1:8000/') 
             .then(function (response) {
-                setKanji(response.data);
+                allKanji = response.data;
+                //setKanji(response.data);
             })
             .catch(function (error) {
                 console.log(error);
             })
             .then(function() {
-                console.log(kanji);
-                const quizList = kanji.sort(() => Math.random() - 0.5).slice(0,6);
+                const quizList = allKanji.sort(() => Math.random() - 0.5).slice(0,6);
                 setQuiz(quizList);
                 setAns(quizList[Math.floor(Math.random()*quizList.length)].kanji);
             })
@@ -25,8 +26,13 @@ const KanjiMap = () => {
 
     function check(answer) {
         if (answer == ans) {
-            console.log("tttt");
-            console.log(kanji);
+            const newQuiz = allKanji.sort(() => Math.random() - 0.5).slice(0,6);
+            console.log(newQuiz);
+            //setQuiz(newQuiz);
+            //setAns(newQuiz[Math.floor(Math.random()*newQuiz.length)].kanji);
+            console.log("Right!");
+        } else {
+            console.log("wrong");
         }
     }
 
@@ -35,7 +41,7 @@ const KanjiMap = () => {
         <h1>Answer: {ans}</h1>
         <div className="kanjiWrapper">
             {quiz.map((k, i) => (
-                <p key={i} onClick={check(k.kanji)} className="kanjiCube">{k.kanji}</p>
+                <p key={i} onClick={() => check(k.kanji)} className="kanjiCube">{k.kanji}</p>
             ))}
         </div>
         </>
