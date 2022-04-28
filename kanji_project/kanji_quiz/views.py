d6204c6bd4ed822f78d007ab9311c6d99cfae7d3
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-28 09:41:33 -0700

    Add files via upload

--- kanji_project/kanji_quiz/views.py
+++ kanji_project/kanji_quiz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from django_filters import rest_framework as filters
 from .serializers import KanjiListSerializer, KanjiGradeSerializer
 from .models import Kanji
 
@@ -7,16 +8,30 @@ class KanjiGradeAPIView(generics.ListAPIView):
     queryset = Kanji.objects.values('grade').distinct()
     serializer_class = KanjiGradeSerializer
 
+class KanjiListAPIView(filters.FilterSet):
+    queryset = Kanji.objects.all()
+    serializer_class = KanjiListSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = {
+        'grade': ["exact"]
+    }
+'''
 class KanjiListAPIView(generics.ListAPIView):
     #queryset = Kanji.objects.all().filter(grade=1) | Kanji.objects.all().filter(grade=3)
     serializer_class = KanjiListSerializer
 
     def get_queryset(self):
+        filter_search = ""
         grade = self.kwargs['grade']
-        print(list(grade))
         for g in grade:
-            print(g)
+            filter_search += ("Kanji.objects.filter(grade={}) | ".format(g))
+        gradex = Kanji.objects.all().filter(*grade)
+        queryset = Kanji.objects.all().filter(grade=1)
+        grades = exec(filter_search[0:-3])
+        print(gradex)
         return Kanji.objects.filter(grade=3)
+'''
+
 '''
 class KanjiListAPIView(generics.ListAPIView):
     queryset = Kanji.objects.all().filter(grade=1)
