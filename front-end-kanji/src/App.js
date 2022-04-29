b8883db8b7bde85e7d6c13bcf436a22c88c0e948
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-29 09:23:28 -0700

    Add files via upload

--- front-end-kanji/src/App.js
+++ front-end-kanji/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 //import KanjiMap from './my_data.js';
 import KanjiMap from './quiz.js';
+import SelectGrade from './grade.js';
 
+  //<KanjiMap grades="23"/>  quizes for Kanji from quiz.js
 function App() {
   return (
     <div className="App">
       <h1 id="kanjiTitle">Kanji Trainer</h1>
       <header className="App-header">
-        <KanjiMap />
       </header>
+      <SelectGrade />
     </div>
   );
 }
---
+++ front-end-kanji/src/grade.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const SelectGrade = () => {
+
+    return (
+        <h1>Choose grade</h1>
+    )
+}
+
+export default SelectGrade;
