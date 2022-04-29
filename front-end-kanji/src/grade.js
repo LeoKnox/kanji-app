1e82c16549b0d1edb12c0f660678372a4ef30e00
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-29 09:33:29 -0700

    Add files via upload

--- front-end-kanji/src/grade.js
+++ front-end-kanji/src/grade.js
@@ -2,9 +2,28 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const SelectGrade = () => {
+    const [grades, setGrades] = [];
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/grades/') 
+            .then(function (response) {
+                console.log(response.data);
+                setGrades(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }, [])
 
     return (
+        <>
         <h1>Choose grade</h1>
+        <div>
+            {grades.map((g, i) => (
+                <p key={i}>{g.grade}</p>
+            ))}
+        </div>
+        </>
     )
 }
 
