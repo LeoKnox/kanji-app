c0f2080e440ee5599a29350ad8a7b4f6f262046b
LeoKnox <LeoKnox@users.noreply.github.com>
2022-05-01 20:08:22 -0700

    Add files via upload

---
+++ front-end-kanji/src/grade2.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const SelectGrade = () => {
+    const [grades, setGrades] = useState([]);
+
+    function selected(e) {
+        axios.post('http://127.0.0.1:8000/g/', {
+            grade: e.target.value
+        })
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+    }
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/g/') 
+            .then(function (response) {
+                console.log(response.data);
+                setGrades(response.data);
+                console.log(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }, [])
+
+    return (
+        <>
+        <h1 className="gradeHeading">Choose grade</h1>
+        <form>
+        {grades.map((g, i) => (
+            <>
+            <label key={i} className="container">{g.grade}
+                <input type="checkbox" name="grade" onClick={selected} value={g.grade} />
+                <span className="checkmark"></span>
+            </label>
+            </>
+        ))}
+        <input className="selectGrade" type="submit" value="Submit" />
+        </form>
+        </>
+    )
+}
+
+export default SelectGrade;
