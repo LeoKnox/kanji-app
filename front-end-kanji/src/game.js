4b1d44c01eab8e52d4ca9b713409e6559b6f218c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-05-10 12:02:34 -0700

    Add files via upload

--- front-end-kanji/src/game.js
+++ front-end-kanji/src/game.js
@@ -1,9 +1,15 @@
 import SelectGrade from './grade2.js';
+import KanjiMap from './quiz.js';
 
 const Game = (meters="grade") => {
     console.log(meters.meters);
     if (meters.meters === "quiz") {
-      return <h3>Quiz ME!!</h3>;
+      return (
+        <>
+        <h3>Quiz ME!!</h3>
+        <KanjiMap grades="2" />
+        </>
+      );
     } else {
       return (
         <div className="App">
