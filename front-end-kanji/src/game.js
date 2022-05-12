501f85437a59db1d9b4b5701e99b7fd5bb89fb39
LeoKnox <LeoKnox@users.noreply.github.com>
2022-05-12 12:53:35 -0700

    Add files via upload

--- front-end-kanji/src/game.js
+++ front-end-kanji/src/game.js
@@ -1,12 +1,16 @@
 import SelectGrade from './grade2.js';
 import KanjiMap from './quiz.js';
+import { useParams } from 'react-router-dom';
 
 const Game = (meters) => {
+    const id = useParams();
+    console.log("ii" + id.id);
     let link = "";
     if (meters.meters ==="home") {
       link = <SelectGrade />;
     } else if (meters.meters === "quiz") {
-      link = <KanjiMap  grades={`${meters.grades}`} />;
+      link = <KanjiMap  grades={`${id.id}`} />;
+      //link = <KanjiMap  grades={`${meters.grades}`} />;
     }
     return (
       <div className="App">
