603abe9d9ed4e2466bae796d5f6f34d162265434
LeoKnox <LeoKnox@users.noreply.github.com>
2022-05-18 13:17:37 -0700

    Add files via upload

--- front-end-kanji/src/game.js
+++ front-end-kanji/src/game.js
@@ -13,7 +13,8 @@ const Game = (meters) => {
       link = <KanjiMap  grades={`${id.id}`} />;
     }
     if (meters.error) {
-      myError = <h3 classId="error">Please select at least one grade.</h3>;
+      myError = document.getElementsByClassName("answer");
+      myError[0].append(<h3 className="error">Please select at least one grade.</h3>);
     }
     return (
       <>
