8238793256fccdd03fd1889feb60bcfee2d2cb4c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-05-19 00:48:15 -0700

    Add files via upload

--- front-end-kanji/src/about.js
+++ front-end-kanji/src/about.js
@@ -1,8 +1,38 @@
 const About = () => {
-    let paragraphOne = <p>
-        This is a response. With more text. And 
-        even more text.
-    </p>;
+    let paragraphOne = 
+    <div>
+        <p>
+        Creating a page to learn Japanese Kanji. I
+        set up a page to select the grade of Kanji
+        of you want.
+        </p>
+
+        <p>
+        Once grade of Kanji is selected user will
+        see a defination and select the correct
+        Kanji.
+        </p>
+
+        <p>
+        Pronunciation and reading of Kanji is shown.
+        User then is shown a new defination and must
+        select a new Kanji.
+        </p>
+
+        <p>
+        Technologies used are MySQL for the database. 
+        Retreiving the grade or grades of Kanji available 
+        to select. Once appropriate grades are selected. The
+        proper Kanji are downloaded to a react front-end.
+        </p>
+
+        <p>
+        Random Kanji are selected and an answer is selected
+        from the those Kanji. It displays the randomly selected
+        Kanji and when user selects correct Kanji is displays
+        a new set of Kanji.
+        </p>
+    </div>;
 
     return (
         <>
